d4eed10fe1b9ceddc01d86817b68f80a41431e97
Tyler Polito <things@tpolito.com>
2023-06-27 09:14:29 -0500

    update config

--- lua/tpolito/remap.lua
+++ lua/tpolito/remap.lua
@@ -1,8 +1,7 @@
 vim.g.mapleader = "'"
--- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>e", function()
-	vim.cmd("vertical resize " .. (50))
-	vim.cmd("Lex")
-end)
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>e", vim.cmd.Lex)
 vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end)
 vim.keymap.set("n", "U", vim.cmd.redo)
