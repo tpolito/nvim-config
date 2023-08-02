b8c06872a6cf2d35cc488bc9ed03b6f8d7fbd76d
Tyler Polito <things@tpolitolcom>
2023-08-02 11:40:41 -0500

    fix for macbook

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,7 +20,8 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['Tab'] = cmp.mapping.select_prev_item(cmp_select),
   ['Shift+Tab'] = cmp.mapping.select_next_item(cmp_select),
-  -- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  -- Don't know why I need this on my personal machine when I don't need it anywhere else
+  ['<CR>'] = cmp.mapping.confirm({ select = true }),
   ["Space"] = cmp.mapping.complete(),
 })
 
@@ -29,7 +30,7 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false } 
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
